5b0227a3d8d9ead8e069d024843380e0baf8cef0
QuirkyDev <joeri.verlooy@gmail.com>
2021-03-27 04:29:28 +0100

    les 3 changes

---
+++ HelloCore/Controllers/BestellingController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloCore.Data;
+using HelloCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloCore.Controllers
+{
+    public class BestellingController : Controller
+    {
+
+        private readonly HelloCoreContext _context;
+
+        public BestellingController(HelloCoreContext context)
+        {
+            _context = context;
+        }
+
+        //Index (bestelling)
+        //Lijst
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bestellingen
+                .Include(b => b.Klant)
+                .ToListAsync());
+        }
+
+        //Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var bestelling = await _context.Bestellingen
+                .Include(b => b.Klant)
+                .FirstOrDefaultAsync(m => m.BestellingID == id);
+            if (bestelling == null)
+            {
+                return NotFound();
+            }
+
+            return View(bestelling);
+        }
+
+        //Toevoegen (bestelling)
+        //GET
+        public IActionResult Create()
+        {
+            ViewBag.KlantenLijst = new SelectList(_context.Klanten, "KlantID", "Naam");
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("BestelllingID, Artikel, Prijs, KlantID")] Bestelling bestelling)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bestelling);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.KlantenLijst = new SelectList(_context.Klanten, "KlantID", "Naam", bestelling.KlantID);
+            return View(bestelling);
+        }
+
+        //Bewerken (bestelling)
+        //GET
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var bestelling = await _context.Bestellingen.FindAsync(id);
+            if (bestelling == null)
+            {
+                return NotFound();
+            }
+            ViewBag.KlantenLijst = new SelectList(_context.Klanten, "KlantID", "Naam", bestelling.KlantID);
+            return View(bestelling);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BestellingID, Artikel, Prijs, KlantID")] Bestelling bestelling)
+        {
+            if (id != bestelling.BestellingID)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bestelling);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BestellingExists(bestelling.BestellingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.KlantenLijst = new SelectList(_context.Klanten, "KlantID", "Naam", bestelling.KlantID);
+            return View(bestelling);
+        }
+
+        //Verwijderen (bestelling)
+        //GET
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bestelling = await _context.Bestellingen
+                .Include(b => b.Klant)
+                .FirstOrDefaultAsync(m => m.BestellingID == id);
+            if (bestelling == null)
+            {
+                return NotFound();
+            }
+
+            return View(bestelling);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bestelling = await _context.Bestellingen.FindAsync(id);
+            _context.Bestellingen.Remove(bestelling);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //BestellingExists
+        private bool BestellingExists(int id)
+        {
+            return _context.Bestellingen.Any(e => e.BestellingID == id);
+        }
+
+    }
+}
